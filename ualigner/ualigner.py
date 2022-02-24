c480669c1c7397ecca1af2ffab86c68b1b76344f
ffreemt <yucongo+fmt@gmail.com>
2022-02-24 15:46:35 +0800

    Update requirments.txt for okteto

--- ualigner/ualigner.py
+++ ualigner/ualigner.py
@@ -8,10 +8,12 @@ try:
     loglevel = int(os.environ.get("loglevel"))
 except:
     loglevel = 20
+_ = """
 if os.environ.get("loglevel").lower() in ["info"]:
     loglevel = 20
 if os.environ.get("loglevel").lower() in ["debug"]:
     loglevel = 10
+# """
 
 logzero.loglevel(loglevel)  # change to 10 to turn on debug
 
